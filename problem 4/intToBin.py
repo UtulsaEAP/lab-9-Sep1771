df9321cfe92551de88f8701bfd7c070245dc797b
sep1771 <sep5990@utulsa.edu>
2024-11-07 14:52:12 -0800

    finished ques6 and completed lab

--- problem 4/intToBin.py
+++ problem 4/intToBin.py
@@ -1,6 +1,9 @@
 def int_to_reverse_binary(num1):
     binary_val = ''
 #write your while loop here
+    if num1 == 0:
+        return '0'
+
     while num1 > 0:
         binary_val += str(num1%2)
         num1//=2
@@ -20,8 +23,8 @@ if __name__ == '__main__':
     user_input = int(input())
     
    
-    binary_string = str(int_to_reverse_binary(user_input))
-    binary_string = str(string_reverse(binary_string))
+    binary_string = (int_to_reverse_binary(user_input))
+    binary_string = (string_reverse(binary_string))
     
     
     print(binary_string)
--- problem 6/fib.py
+++ problem 6/fib.py
@@ -1,8 +1,21 @@
 def fibonacci(n):
     
     #write your code here
-    return
+    
+    list_fib = [0,1]
+
+    fib_counter = 0 
+    while fib_counter <= n:
+
+        list_fib.append(list_fib[fib_counter] + list_fib[fib_counter + 1])
+
+        fib_counter += 1
+
+
+
+    return list_fib[n]
 
 if __name__ == '__main__':
     start_num = int(input())
+
     print(f'fibonacci({start_num}) is {fibonacci(start_num)}')
